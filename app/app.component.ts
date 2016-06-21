493e65f2ed29fba1c304c2c5e462e1a924f7877f
pietro909 <pietro.grandi.3d@gmail.com>
2016-06-21 09:15:29 +0200

    application is running

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component }       from '@angular/core';
+
+@Component({
+
+    selector: 'my-app',
+
+    template: `<h1>{{title}}</h1>`,
+
+})
+
+export class AppComponent {
+
+    title = 'One Source of Truth for Angular 2';
+
+}
---
+++ app/main.ts
@@ -0,0 +1,9 @@
+import { provide } from '@angular/core';
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { HTTP_PROVIDERS } from '@angular/http';
+
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent, [
+    HTTP_PROVIDERS
+]);
