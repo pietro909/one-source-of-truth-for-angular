d182c0bc8374d640de6f24065a96f32c3f78e41e
pigr2 <pietro.grandi@betssongroup.com>
2016-09-16 17:32:49 +0200

    search box is broken, but all the stuff is running! YEAH! ...and btw, I didn't go home :-)

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,45 +5,39 @@ import {Store} from "@ngrx/store";
 import {CurrentSearch} from "./models/current-search.model";
 import {SearchResult} from "./models/search-result.model";
 import {YouTubeService} from "./services/youtube.service";
-import {SearchBox} from "./components/search-box.component";
-import {ProximitySelector} from "./components/proximity-selector.component";
-
-/*
- <div class="row col-md-8">
- <search-box [store]="store"></search-box>
- <proximity-selector [store]="store" [disabled]="disableSearch"
- [ngClass]="{ disabled: disableSearch }"></proximity-selector>
- </div>
- <div class="row col-md-8 alert alert-danger" *ngIf="disableSearch">
- <p>Can't use geolocalization with an empty searchbox</p>
- </div>
- <div class="row col-md-8">
- <p>
- Try to type something in the searchbox, play with the location and with radius: the above state will
- always be consistent and up to date.
- </p>
- <p class="state">{{ state | json }}</p>
- <p class="state" *ngIf="disableSearch">{ }</p>
- </div>
- <h2 *ngIf="!disableSearch">Search results:</h2>
- <h2 *ngIf="disableSearch || searchResults.length == 0">No results</h2>
- <div class="row col-md-8">
- <div *ngFor="let result of searchResults" class="thumbnail col-sm-6 col-md-4">
- <div class="caption">
- <h3>{{ result.title }}</h3>
- </div>
- <!--<img src="{{ result.thumbnailUrl }}" />-->
- </div>
- </div>
- */
 
 @Component({
     selector: 'my-app',
     template: `
     <section class="col-md-8">
         <h1>{{title}}</h1>
-
-    </section>
+        <div class="row col-md-8">
+            <search-box [store]="store"></search-box>
+            <proximity-selector [store]="store" [disabled]="disableSearch"
+            [ngClass]="{ disabled: disableSearch }"></proximity-selector>
+        </div>
+        <div class="row col-md-8 alert alert-danger" *ngIf="disableSearch">
+            <p>Can't use geolocalization with an empty searchbox</p>
+        </div>
+        <div class="row col-md-8">
+            <p>
+            Try to type something in the searchbox, play with the location and with radius: the above state will
+            always be consistent and up to date.
+            </p>
+            <p class="state">{{ state | json }}</p>
+            <p class="state" *ngIf="disableSearch">O</p>
+        </div>
+        <h2 *ngIf="!disableSearch">Search results:</h2>
+        <h2 *ngIf="disableSearch || searchResults.length == 0">No results</h2>
+        <div class="row col-md-8">
+            <div *ngFor="let result of searchResults" class="thumbnail col-sm-6 col-md-4">
+                <div class="caption">
+                <h3>{{ result.title }}</h3>
+                </div>
+                <!--<img src="{{ result.thumbnailUrl }}" />-->
+            </div>
+        </div>
+        </section>
     `
 })
 export class AppComponent implements OnInit {
