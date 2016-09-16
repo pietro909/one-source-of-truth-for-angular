525f922d5ee82d204169e15618cb187dfde21c37
pigr2 <pietro.grandi@betssongroup.com>
2016-09-16 10:57:08 +0200

    quickstart added and working... need to bring everything there. I'll do it in the afternoon :-)

--- systemjs.config.js
+++ systemjs.config.js
@@ -23,11 +23,11 @@
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
-        'app':                        { main: 'main.js',  defaultExtension: 'js' },
-        'rxjs':                       { defaultExtension: 'js' },
-        'angular2-in-memory-web-api': { main: './index.js', defaultExtension: 'js' },
-        '@ngrx/core': { main: 'index.js', defaultExtension: 'js' },
-        '@ngrx/store': { main: 'index.js', defaultExtension: 'js' }
+        'app':                          { main: 'main.js',  defaultExtension: 'js' },
+        'rxjs':                         { defaultExtension: 'js' },
+        'angular2-in-memory-web-api':   { main: './index.js', defaultExtension: 'js' },
+        '@ngrx/core':                   { main: 'index.js', defaultExtension: 'js' },
+        '@ngrx/store':                  { main: 'index.js', defaultExtension: 'js' }
     };
     var config = {
         paths: paths,
