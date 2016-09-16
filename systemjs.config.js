94f546668031bade684ff9beb4508685adf4be9a
pigr2 <pietro.grandi@betssongroup.com>
2016-09-16 10:36:40 +0200

    upgrade to 2.0 as per official docs

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,34 +1,36 @@
 (function(global) {
+    var paths = {
+        // paths serve as alias
+        'npm:': 'node_modules/'
+    };
     // map tells the System loader where to look for things
     var map = {
         'app':                        'dist',
-        '@angular':                   'node_modules/@angular',
-        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
-        'rxjs':                       'node_modules/rxjs',
-        '@ngrx':                      'node_modules/@ngrx'
+
+        // angular bundles
+        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+        'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+        'rxjs':                       'npm:rxjs',
+        '@ngrx':                      'npm:@ngrx'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
         'app':                        { main: 'main.js',  defaultExtension: 'js' },
-        'rxjs':                       { defaultExtension: 'js' }
+        'rxjs':                       { defaultExtension: 'js' },
+        'angular2-in-memory-web-api': { main: './index.js', defaultExtension: 'js' },
+        '@ngrx/core': { main: 'index.js', defaultExtension: 'js' },
+        '@ngrx/store': { main: 'index.js', defaultExtension: 'js' }
     };
-    var packageNames = [
-        '@angular/common',
-        '@angular/compiler',
-        '@angular/core',
-        '@angular/http',
-        '@angular/platform-browser',
-        '@angular/platform-browser-dynamic',
-        '@angular/testing',
-        '@angular/upgrade',
-        '@ngrx/core',
-        '@ngrx/store'
-    ];
-    // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
-    packageNames.forEach(function(pkgName) {
-        packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
-    });
     var config = {
+        paths: paths,
         map: map,
         packages: packages
     };
