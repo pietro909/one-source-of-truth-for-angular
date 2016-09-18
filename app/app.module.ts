33da0ed7fe70f28326a4a4c1a0c2bbbae435499e
Pietro909 <pietro.grandi.3d@gmail.com>
2016-09-18 15:54:43 +0200

    some minor code clean

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,18 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import {AppComponent} from "./app.component";
-import {Store, StoreModule} from "@ngrx/store";
-import {SearchReducer} from "./reducers/search.reducer";
-import {YouTubeService} from "./services/youtube.service";
+import {NgModule}      from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 import {HttpModule} from "@angular/http";
-import {SearchBox} from "./components/search-box.component";
-import {ProximitySelector} from "./components/proximity-selector.component";
+import {Store, StoreModule} from "@ngrx/store";
 
-const storeManager = StoreModule.provideStore ({ currentSearch: SearchReducer });
+import {AppComponent} from "./app.component";
+import {YouTubeService} from "./services/youtube.service";
+import {ProximitySelector} from "./components/proximity-selector.component";
+import {SearchBox} from "./components/search-box.component";
+import {SearchReducer} from "./reducers/search.reducer";
+
+const storeManager = StoreModule.provideStore({ currentSearch: SearchReducer });
 
 @NgModule({
-    imports:      [ BrowserModule, StoreModule, storeManager, HttpModule ],
+    imports:      [ BrowserModule, HttpModule, StoreModule, storeManager ],
     declarations: [ AppComponent, SearchBox, ProximitySelector ],
     bootstrap:    [ AppComponent ],
     providers:    [ YouTubeService ]
