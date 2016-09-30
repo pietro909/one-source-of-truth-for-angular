5357a11c478dee21bc151964c39305238cd12be8
Pietro909 <pietro.grandi.3d@gmail.com>
2016-09-30 12:11:45 +0200

    fix centering, add github ribbon

--- app/app.component.ts
+++ app/app.component.ts
@@ -28,7 +28,9 @@ import {YouTubeService} from "./services/youtube.service";
             <p class="state" *ngIf="disableSearch">state is empty</p>
             <h2 *ngIf="!disableSearch">Search results:</h2>
         </div>
-        <h2 *ngIf="disableSearch || searchResults.length == 0">No results</h2>
+        <div class="row col-md-8">
+            <h2 *ngIf="disableSearch || searchResults.length == 0">No results</h2>
+        </div>
         <div class="row col-md-8">
             <div *ngFor="let result of searchResults" class="thumbnail col-sm-6 col-md-4">
                 <div class="caption">
